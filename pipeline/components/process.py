7014bf3a0c46207131ea64dc0e0b08e2496cc2c2
Nathan Kim <kimnathan0324@gmail.com>
2025-11-04 15:25:37 -0500

    modularized process.py, small reformatting and fixes to both preprocessing and standarize classes

--- pipeline/components/process.py
+++ pipeline/components/process.py
@@ -1,18 +1,177 @@
 from pipeline.base import Component, DataWrapper
+from standardize import Standardize
+import os
+import pandas as pd
+from datetime import timedelta
 
 class Processing(Component):
-    def __init__(self, name):
+    def __init__(self, name, state, date):
         super.__init__(name)
-        # initialze everything else
+        self.state = state
+        self.date = date
+        self.schema = [
+            'ID', 
+            'county', 
+            'daily_max_customers_affected', 
+            'per_outage_customers_affected', 
+            'customers_served',
+            'start_time',
+            'end_time',
+            'duration'
+        ]
+        self.std = Standardize(name="", state=f"{self.state}", date=f"{self.date}")
+        self.col_map = self.std.get_col_map()           # {raw col name : std col name}
+        self.col_lists = self.std.get_all_col_lists()   # {std col name : [all raw cols]}
+        self.county_map = self.std.get_county_map()     # {raw county name : std county name}
+        self.raw_county_list = self.std.get_raw_county_list()        # [county 1, county 2, ...]
+        self.master_county_list = self.std.get_master_county_list()  # [county 1, county 2, ...]
+        self.county_dfs = {c: pd.DataFrame(columns=self.schema) for c in self.master_county_list}
 
-    def standardize(self, data):
-        # example helper function 
-        return None
+
+
+    # Aggregate data for a given county and provider and add it to the list of dfs to return 
+    def aggregate(self, df, county):
+        # Get only the columns we need 
+        df = df[['county', 'per_outage_customers_afffected', 'customers_served', 'timestamp']]
+
+        # Setup for outage grouping
+        df = df.sort_values('timestamp')
+        last_id = self.county_dfs[county]['ID'].max() if not self.county_dfs[county].empty else 0
+        threshold = timedelta(hours=1, minutes=14)
+
+        # Group by timestamp and set IDs for each outage
+        df['diff'] = df['timestamp'].diff()             # Get the time difference of curr - previous
+        mask = df['diff'] > threshold                   
+        df['new_outage'] = (df['diff'].isna() | mask)   # Classify the start of a new outage 
+        df['ID'] = df['new_outage'].cumsum() + last_id  # Updates the ID using each new outage to increment ID
+
+        # Aggregate result
+        result = (
+            df.groupBy('ID').agg(
+                county=('county', 'first'),
+                per_outage_customers_affected=('per_outage_customers_affected', 'max'),
+                customers_served=('customers_served', 'max'),
+                start_time=('timestamp', 'min'),
+                end_time=('timestamp', 'max')
+            ).reset_index()
+        )
+
+        result['daily_max_customers_affected'] = 0
+        result['duration'] = result['end_time'] - result['start_time']
+
+        if self.county_dfs[county].empty:
+            self.county_dfs[county] = result
+        else:
+            self.county_dfs[county] = pd.concat([self.county_dfs[county], result], ignore_index=True)
+
+
+
+    # Fill in the daily max customers affected column for each county
+    def fill_daily_max(self, county):
+        # Convert customers_affected to numeric 
+        self.county_dfs[county]['per_outage_customers_affected'] = pd.to_numeric(
+            self.county_dfs[county]['per_outage_customers_affected'],
+            errors='coerce'
+        )
+
+        if self.county_dfs[county]['per_outage_customers_affected'].sum() > 0:
+            daily_max = self.county_dfs[county]['per_outage_customers_affected'].max()
+            self.county_dfs[county]['daily_max_customers_affected'] = daily_max
+
+
+
+    # Creates filler dataframes for counties that had no reported outages for a given day
+    def create_filler(self, county):
+        # Pull historical customers served
+        read_path = os.path.join(self.std.base_path, 'historical.csv')
+        df = pd.read_csv(read_path)
+        county_df = df.loc[df['county'] == county]
+        historical_val = -1
+
+        # Check that the historical data exists
+        if not county_df.empty:
+            historical_val = int(county_df['customers_served'].sum())
+        else:
+            print(f"Unable to find historical data for {county}, {self.state}")
+        
+        # Create the filler dataframe using the historical data
+        result = pd.DataFrame([{
+            "ID": 1,
+            "county": county,
+            "daily_max_customers_affected": 0,
+            "per_outage_customers_afffected": 0,
+            "customers_served": historical_val,
+            "start_time": pd.NaT,
+            "end_time": pd.NaT,
+            "duration": pd.Timedelta(0)
+        }], columns=self.schema)
+
+        self.county_dfs[county] = result
+        # Reindex to reorganize and enforce column order
+        self.county_dfs[county] = self.county_dfs[county].reindex(columns=self.schema)
+
+
+
+    # Sums each provider's customers served number to be used as the total county customers served
+    def aggregate_customers_served(self, county):
+        # Enforce numeric datatype
+        self.county_dfs[county]['customers_served'] = pd.to_numeric(
+            self.county_dfs[county]['customers_served'],
+            errors='coerce'
+        ).fillna(0)
+
+        # Keep only 1 instance of each unique EMC, take the sum, replace previous customers served with correct values
+        unique_customers_served = self.county_dfs[county]['customers_served'].drop_duplicates()
+        sum = int(unique_customers_served.unique().sum())
+        self.county_dfs[county]['customers_served'] = sum
+
+
+
+    def process(self, data):
+        # Add initial state of processed data to self.county_dfs dictionary
+        for i, df in enumerate(data):
+            # Standardize data
+            df.columns = df.columns.str.strip().str.lower()
+            res = self.std.standardize(df)
+
+            # Check that the data is valid
+            if res[0]:
+                df = res[1]
+            else:
+                print("The following columns were missing: ")
+                print(res[1])
+                continue
+
+            # Process each county
+            for county in self.raw_county_list:
+                county_df = df[df['county'] == county]
+
+                if county_df.size != 0:
+                    self.aggregate(county_df, county)
+
+        # Fill in daily max values for each county
+        for county in self.county_dfs:
+            if not self.county_dfs[county].empty:
+                self.fill_daily_max(county)
+
+        # Create filler dataframes for counties with no reported outages 
+        for county in self.master_county_list:
+            if self.county_dfs[county].empty:
+                self.create_filler(county)
+
+        # Aggregate provider level customers served data into county level data
+        for county in self.county_dfs:
+            if not self.county_dfs[county].empty:
+                self.aggregate_customers_served(county)
+
+        return self.county_dfs
     
+
+
     def run(self, data):
         # this is the main function to be implemented, use your helper functions here 
-        normalized_data = self.standardize(data)
+        processed_data = self.process(data)
         # once you have data ready for the next step, now we wrap it using the DataWrapper Class
-        per_county_data = DataWrapper(normalized_data)
+        per_county_data = DataWrapper(processed_data)
         # we can return this data to the next component of the pipeline
         return per_county_data
--- pipeline/components/standardize.py
+++ pipeline/components/standardize.py
@@ -1,15 +1,77 @@
 from pipeline.base import Component, DataWrapper
 import numpy as np
 import pandas as pd
+import os
+import csv
 
 class Standardize(Component):
-    def __init__(self, name, state, col_map, col_lists, dupe_dict):
+    def get_col_map(self):
+        col_map = {}
+        read_path = os.path.join(self.base_path, "col_map.csv")
+        
+        with open(read_path) as f:
+            reader = csv.reader(f)
+            header = next(reader)
+
+            for row in reader:
+                raw = row[0]
+                std_name = row[1]
+                col_map[raw] = std_name
+        
+        return col_map
+
+    def get_all_col_lists(self):
+        read_path = os.path.join(self.base_path, "raw_col_lists.csv")
+        df = pd.read_csv(read_path)
+        col_lists = {col: df[col].toList() for col in df.columns}
+
+        return col_lists
+
+    def get_county_map(self):
+        county_map = {}
+        read_path = os.path.join(self.base_path, "county_map.csv")
+
+        with open(read_path) as f:
+            reader = csv.reader(f)
+            header = next(reader)
+
+            for row in reader:
+                raw = row[0]
+                std_name = row[1]
+                county_map[raw] = std_name
+            
+        return county_map
+
+    def get_raw_county_list(self):
+        raw_county_list = set()
+        read_path = os.path.join(self.base_path, "raw_county_list.txt")
+
+        with open(read_path) as f:
+            for line in f:
+                raw_county_list.add(line.str.strip().str.lower())
+        
+        return raw_county_list
+
+    def get_master_county_list(self):
+        master_county_list = set()
+        read_path = os.path.join(self.base_path, "master_county_list.txt")
+
+        with open(read_path) as f:
+            for line in f:
+                master_county_list.add(line.str.strip().str.lower())
+        
+        return master_county_list
+
+    
+
+    def __init__(self, name, state, date):
         super.__init__(name)
         self.state = state
-        self.col_map = col_map
-        self.col_lists = col_lists
-        self.dupe_dict = dupe_dict
-        # initialze everything else
+        self.base_path = os.path.join("pipeline\\mappings", f"{self.state}")
+        self.col_map = self.get_col_map()           # {raw col name : std col name}
+        self.col_lists = self.get_all_col_lists()   # {std col name : [all raw cols]}
+        self.county_map = self.get_county_map()     # {raw county name : std county name}
+        self.date = date
 
     # Standardizes column names and drops redundant / unnecessary columns
     def rename_cols(self, data):
@@ -85,11 +147,13 @@ class Standardize(Component):
             ).fillna(0)
         )
 
+        data['timestamp'] = pd.to_datetime(data['timestamp'], errors='coerce')
+        data = data[data['timestamp'].dt.date == pd.to_datetime(self.date).date()]
+
         return data
 
     # Standardizes county names
     def standardize_county(self, data):
-        county_dict = self.dupe_dict[self.state]
         data = data.copy()
         # Map raw county names to standardized names
         data['county'] = (
@@ -97,7 +161,7 @@ class Standardize(Component):
             .astype(str)
             .str.strip()
             .str.lower()
-            .map(county_dict)
+            .map(self.county_map)
         )
         # Standardize datatype
         data = data[data['county'].map(type) == str]
@@ -130,14 +194,14 @@ class Standardize(Component):
                 skip = True
         
         if skip:
-            return [False, good_data]
+            return [False, missing]
 
         # Standardize data types
         type_std_data = self.standardize_data_types(good_data)
         # Standardize county names
         std_data = self.standardize_county(type_std_data)
 
-        return std_data
+        return [True, std_data]
 
     def run(self, data):
         normalized_data = self.standardize(data)
