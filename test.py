f59b78a23add4998c45e8c97895756640aec4ec4
Nathan Kim <kimnathan0324@gmail.com>
2026-01-26 20:40:45 -0500

    add std / proc testing file, updated lower & upper bound aggregation

---
+++ test.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+from pprint import pprint
+from pipeline.base import Component, DataWrapper
+from pipeline.components.standardize import Standardize
+from pipeline.components.process import Processing
+
+
+df = pd.read_csv("testing/al_outages_past_year.csv")
+# pprint(df)
+
+STATE = "AL"
+DATE = "2024-11-21"
+
+# std = Standardize(
+#     name="test",
+#     state=STATE,
+#     date=DATE
+# )
+# TYPE = "std"
+# res = std.standardize(df)
+# combined = res[1]
+
+proc = Processing(
+    name="test",
+    state=STATE,
+    date=DATE
+)
+TYPE = "proc"
+res = proc.process([df])
+combined = pd.concat(res.values(), ignore_index=True)
+
+combined.to_csv(os.path.join("testing", f"{TYPE}_{STATE}_all_counties_{DATE}.csv"), index=False)
